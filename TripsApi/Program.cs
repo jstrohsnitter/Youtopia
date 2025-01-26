605ef14e283934253ca390358d29117db9aa4df4
jstrohsnitter <jstrohsnitter@gmail.com>
2025-01-26 14:51:50 -0500

    allow all method heards added

--- TripsApi/Program.cs
+++ TripsApi/Program.cs
@@ -1,10 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using TripApi.Models;
 
+
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        builder =>
+        {
+            builder.WithOrigins(
+                "https://localhost:5098", "*")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+        });
+});
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TripContext>(opt =>
     opt.UseInMemoryDatabase("TripList"));
@@ -21,10 +34,44 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// app.MapGet("/", () => "Hello World!");
+
+// app.MapGet("/trips", async(TripDb db) => await db.Trips.ToListAsync());
+
+// app.MapPost("/pizzas", async(PizzaDb db, Pizza pizza) => {
+//     await db.Pizzas.AddAsync(pizza);
+//     await db.SaveChangesAsync();
+//     return Results.Created($"/pizzas/{pizza.Id}", pizza);
+// });
+
+// app.MapPut("/pizzas/{id}", async (PizzaDb db, Pizza updatePizza, int id) =>
+// {
+//   var pizzaItem = await db.Pizzas.FindAsync(id);
+//   if (pizzaItem is null) return Results.NotFound();
+//   pizzaItem.Name = updatePizza.Name;
+//   pizzaItem.Description = updatePizza.Description;
+//   await db.SaveChangesAsync();
+//   return Results.NoContent();
+// });
+
+// app.MapDelete("/pizzas/{id}", async (PizzaDb db, int id) =>
+// {
+//   var todo = await db.Pizzas.FindAsync(id);
+//   if (todo is null)
+//   {
+//     return Results.NotFound();
+//   }
+//   db.Pizzas.Remove(todo);
+//   await db.SaveChangesAsync();
+//   return Results.Ok();
+// });
 
 app.Run();
